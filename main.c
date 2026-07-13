3b14bee18a3c8315ac264e0e18db589d9c13ef2e
newton <mwauranewton99@gmail.com>
2026-07-14 02:06:02 +0300

    try to match the length into a square length

--- main.c
+++ main.c
@@ -51,9 +51,22 @@ int main(int argc, char *argv[]){
 			offset = strlen(target_data);
 			target_data[offset] = '\n';
 			offset++;
-		 }
-	//	printf("we read >>>>>>>>>>>>>>>%s<<<<<<<<<<<<<", target_data);
-	printf("size of the data we did read is %d.\n", strlen(target_data));
+		}
+		printf("size of the data we did read is %d.\n", strlen(target_data));
+		int margin = strlen(key) - strlen(target_data) % strlen(key);
+
+		printf("MARGIN FOUND IS %d.\n", margin);
+		final_size = strlen(target_data) + margin;
+		printf("%d converts to %d\n", strlen(target_data), final_size);
+		
+		int i = 0;
+		do{
+			//memset(target_data + strlen(target_data), '`', margin);
+			target_data[strlen(target_data) + i] = '~';
+			i++;
+		}while(i <= margin);
+		target_data[final_size] = '\0';
+		printf("we read >>>>>>>>>>>>>>>%s<<<<<<<<<<<<<", target_data);
 	}else{
 		/* Things here will move away from stack and go to heap
 		 * for easier management and simplicity 
@@ -61,19 +74,27 @@ int main(int argc, char *argv[]){
 		target_fd = open(target_data, O_RDONLY);
 		struct stat file_size ;
 		fstat(target_fd, &file_size);
-		int actual_size = file_size.st_size - 1;
-		printf("The size is of %d bytes.\n", actual_size);
-
-		final_size = (actual_size +  (strlen(key) - actual_size % strlen(key))) + 1;
-		printf("%d converts to %d\n", actual_size, final_size);
-		target_data = mmap(NULL,final_size, PROT_READ, MAP_PRIVATE, target_fd, 0 );
+		int actual_size = file_size.st_size ;
+		int margin = strlen(key) - actual_size % strlen(key);
+		final_size = actual_size + margin; 
+		// printf("%d converts to %d\n", actual_size, final_size);
+		/* The above statement is a source of many questions
+		 * that after a well analysis will prove code entirely
+		 * correct or entirely wrong 
+		 * in the area of reading data
+		 */
+		target_data = mmap(NULL,4096, PROT_READ, MAP_PRIVATE, target_fd, 0 );
 		if(target_data != NULL)
 			fprintf(stderr, "Success mapping our file.\n");
-
+#if 0
+		printf("MARGIN FOUND IS %d.\n", margin);
+		printf("%d converts to %d\n", strlen(target_data), final_size);
+		printf("mapped file size is %d bytes.\n", strlen(target_data));
+#endif
+		strcpy(target_data, "```````````````");
+		 printf("we read >>>>>>>>>>>>>>>%s<<<<<<<<<<<<<", target_data);
 	}
 
-	/* Page size of our machine */
-	printf("The page size of this machine is %d.\n", sysconf(_SC_PAGESIZE));
 	char *cipher_text = (char *)malloc(final_size * sizeof(char));
 	free(cipher_text);
 	return 0;
