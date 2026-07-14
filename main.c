d85650e3a38f7d43d83d1315de2a6e598367f934
newton <mwauranewton99@gmail.com>
2026-07-14 03:35:53 +0300

    structured version

--- main.c
+++ main.c
@@ -7,14 +7,22 @@
 #include <unistd.h>
 #include <ctype.h>
 
+/* variables */
+char *key = NULL;
+char *filename;
+struct stat file_size;
+char *target_data;
+int argument;
+bool should_we_countinue = false;
+bool using_file = false;
+bool is_unmap = false;
+
+/* functions */
+void obtain_plain_text();
+void finalize();
+void transpose();
 
 int main(int argc, char *argv[]){
-	char *key = NULL;
-	char *filename;
-	char *target_data;
-	int argument;
-	bool should_we_countinue = false;
-	bool using_file = false;
 
 	while((argument = getopt(argc, argv, ":k:f:")) != -1){
 		switch(argument){
@@ -37,6 +45,13 @@ int main(int argc, char *argv[]){
 	/* if we don't have the key then why continue */
 	if(!should_we_countinue) return 1;
 
+	obtain_plain_text();
+	transponse();
+	finalize();
+	return 0;
+}
+
+void obtain_plain_text(){
 	// printf("we abtained the key :: %s.\n", key);
 	if(!using_file){
 		/* Here things are going on an array of characters
@@ -51,7 +66,6 @@ int main(int argc, char *argv[]){
 		 * for easier management and simplicity 
 		 */
 		int fd = open(filename, O_RDONLY);
-		struct stat file_size;
 		fstat(fd, &file_size);
 
 		target_data = mmap(NULL, file_size.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
@@ -60,9 +74,20 @@ int main(int argc, char *argv[]){
 			return 1;
 		}
 
+		close(fd);
 	}
+}
 
+void transpose(){
+/* the whole logic */
 	char *cipher_text = (char *)malloc(4096);
+
 	free(cipher_text);
-	return 0;
+
+}
+void finalize(){
+	if(is_unmap)
+		munmap(target_data, file_size.st_size);
+	else
+		free(target_data);
 }
